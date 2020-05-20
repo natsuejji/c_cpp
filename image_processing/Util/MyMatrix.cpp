9e4507a9c800d1efe421e69481629573f259986e
lee yihan <natsuejji1@gmail.com>
2020-05-21 01:01:52 +0800

    加入未完成的影像處理程式

---
+++ image_processing/Util/MyMatrix.cpp
@@ -0,0 +1,60 @@
+#include <MyMatrix.hpp>
+//隨機初始化陣列
+void MyMatrix::RandomInitData()
+{
+}
+//轉出mat
+cv::Mat MyMatrix::VToMat()
+{
+    cv::Mat n = cv::Mat(this->data);
+    return n.reshape(3, this->rows);
+}
+
+//data
+vector<float> MyMatrix::GetData()
+{
+    return this->data;
+}
+//使用外來cv::mat作為輸入
+void MyMatrix::SetData(cv::Mat input)
+{
+    int rows = input.rows;
+    int cols = input.cols;
+    int channel = input.channels();
+    cv::Mat imagef;
+    input.convertTo(imagef, CV_32FC3, 1 / 255.0);
+    this->data = (vector<float>)imagef.reshape(1, 1);
+}
+
+void MyMatrix::SetData(string imagePath)
+{
+    cv::Mat image = cv::imread(imagePath);
+    int rows = image.rows;
+    int cols = image.cols;
+    int channel = image.channels();
+    cv::Mat imagef;
+    image.convertTo(imagef, CV_32FC3, 1 / 255.0);
+    this->data = (vector<float>)imagef.reshape(1, 1);
+}
+//設定外來vector為data
+void MyMatrix::SetData(vector<float> data, int rows, int cols)
+{
+    this->data = data;
+    this->rows = rows;
+    this->cols = cols;
+}
+//Rows
+int MyMatrix::GetRows()
+{
+    return this->rows;
+}
+//Cols
+int MyMatrix::GetCols()
+{
+    return this->cols;
+}
+//channels
+int MyMatrix::GetChannels()
+{
+    return this->channels;
+}
---
+++ image_processing/Util/MyMatrix.hpp
@@ -0,0 +1,70 @@
+
+//儲存矩陣資料用
+#ifndef MYMATRIX_H
+#define MYMATRIX_H
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+using namespace std;
+
+class MyMatrix
+{
+private:
+    /* data */
+    int rows;
+    int cols;
+    int channels;
+    vector<float> data;
+
+public:
+    //constructor
+    MyMatrix(cv::Mat input);
+    MyMatrix(int rows, int cols, int channels);
+    MyMatrix(const MyMatrix &copy);
+    //destructor
+    ~MyMatrix();
+
+    //隨機初始化陣列
+    void RandomInitData();
+    //轉出mat
+    cv::Mat VToMat();
+
+    //data
+    vector<float> GetData();
+    //直接讀取影像
+    void SetData(string imagePath);
+    //使用mat作為輸入
+    void SetData(cv::Mat input);
+    //使用外來vec作為輸入
+    void SetData(vector<float> data, int rows, int cols);
+    //Rows
+    int GetRows();
+    //Cols
+    int GetCols();
+    //channels
+    int GetChannels();
+};
+//constructor
+MyMatrix::MyMatrix(cv::Mat input)
+{
+    this->SetData(input);
+}
+
+MyMatrix::MyMatrix(int rows, int cols, int channels)
+{
+    this->rows = rows;
+    this->cols = cols;
+    this->channels = channels;
+    this->RandomInitData();
+}
+
+MyMatrix::MyMatrix(const MyMatrix &copy)
+{
+}
+//destructor
+MyMatrix::~MyMatrix()
+{
+}
+#endif
